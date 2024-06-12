d38d0ebdd1c3b4d399f44f5b99aa8be083600cfe
Francis Nikko M. Santiago <nikko.santiago@coreproc.com>
2024-06-12 15:07:20 +0800

    Add project files.

---
+++ Restaurants.API/Controllers/GenerateWeatherForecastDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Restaurants.API.Controllers;
+
+public class GenerateWeatherForecastDto
+{
+	public double MinTemp { get; set; }
+	public double MaxTemp { get; set; }
+}
---
+++ Restaurants.API/Controllers/IWeatherForecastService.cs
@@ -0,0 +1,7 @@
+﻿
+namespace Restaurants.API.Controllers;
+
+public interface IWeatherForecastService
+{
+	IEnumerable<WeatherForecast> Get(int take, double minTemp, double maxTemp);
+}
