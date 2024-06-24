27c2327cc66b0579bc458747db99fb840ada377d
Francis Nikko M. Santiago <nikko.santiago@coreproc.com>
2024-06-24 22:40:38 +0800

    Section 8 Create Dish Response Header

--- Restaurants.Application/Dishes/Commands/CreateDish/CreateDishCommand.cs
+++ Restaurants.Application/Dishes/Commands/CreateDish/CreateDishCommand.cs
@@ -1,7 +1,7 @@
 ﻿using MediatR;
 
 namespace Restaurants.Application.Dishes.Commands.CreateDish;
-public class CreateDishCommand : IRequest
+public class CreateDishCommand : IRequest<int>
 {
 	public string Name { get; set; } = default!;
 	public string Description { get; set; } = default!;
