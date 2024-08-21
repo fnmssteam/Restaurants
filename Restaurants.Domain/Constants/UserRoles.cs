448b4b086b6c1ed0cfb3419d450e89f14919179d
Francis Nikko M. Santiago <nikko.santiago@coreproc.com>
2024-08-22 04:50:01 +0800

    Section 10 Role based authorization

---
+++ Restaurants.Domain/Constants/UserRoles.cs
@@ -0,0 +1,7 @@
+﻿namespace Restaurants.Domain.Constants;
+public static class UserRoles
+{
+	public const string User = "User";
+	public const string Owner = "Owner";
+	public const string Admin = "Admin";
+}
