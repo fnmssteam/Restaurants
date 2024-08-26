4eb65f081b838f091187038cedc235aab161b416
Francis Nikko M. Santiago <nikko.santiago@coreproc.com>
2024-08-26 10:15:53 +0800

    Section 12 Unit test for protected or internal classes

--- Restaurants.Infrastructure/Authorization/Requirements/CreatedMultipleRestaurantsRequirementHandler.cs
+++ Restaurants.Infrastructure/Authorization/Requirements/CreatedMultipleRestaurantsRequirementHandler.cs
@@ -1,18 +1,14 @@
 ﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Logging;
 using Restaurants.Application.Users;
 using Restaurants.Domain.Repositories;
 
 namespace Restaurants.Infrastructure.Authorization.Requirements;
-internal class CreatedMultipleRestaurantsRequirementHandler(ILogger<CreatedMultipleRestaurantsRequirementHandler> logger,
-	IRestaurantsRepository restaurantsRepository,
+internal class CreatedMultipleRestaurantsRequirementHandler(IRestaurantsRepository restaurantsRepository,
 	IUserContext userContext) : AuthorizationHandler<CreatedMultipleRestaurantsRequirement>
 {
 	protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, CreatedMultipleRestaurantsRequirement requirement)
 	{
 		var currentUser = userContext.GetCurrentUser();
-
-		logger.LogInformation("User: {Email} - Handling CreatedMultipleRestaurantsRequirement", currentUser.Email);
 
 		var restaurants = await restaurantsRepository.GetAllAsync();
 
