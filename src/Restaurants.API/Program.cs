65642aa7dbfcc7bd761a2dda3408d5572c7515c6
Francis Nikko M. Santiago <nikko.santiago@coreproc.com>
2024-08-27 03:21:53 +0800

    Section 13 Connection to Azure SQL & automatic migrations, Application insights

--- src/Restaurants.API/Program.cs
+++ src/Restaurants.API/Program.cs
@@ -6,46 +6,57 @@ using Restaurants.Infrastructure.Extensions;
 using Restaurants.Infrastructure.Seeders;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.AddPresentation();
-
-builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddApplication();
-
-var app = builder.Build();
-
-var scope = app.Services.CreateScope();
-var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
-
-await seeder.Seed();
-
-// Configure the HTTP request pipeline. Note: order is important
-app.UseMiddleware<ErrorHandlingMiddleware>();
-app.UseMiddleware<RequestTimeLoggingMiddleware>();
-
-app.UseSerilogRequestLogging();
-
-if (app.Environment.IsDevelopment())
+try
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+	var builder = WebApplication.CreateBuilder(args);
+
+	// Add services to the container.
+
+	builder.AddPresentation();
+
+	builder.Services.AddInfrastructure(builder.Configuration);
+	builder.Services.AddApplication();
+
+	var app = builder.Build();
+
+	var scope = app.Services.CreateScope();
+	var seeder = scope.ServiceProvider.GetRequiredService<IRestaurantSeeder>();
+
+	await seeder.Seed();
+
+	// Configure the HTTP request pipeline. Note: order is important
+	app.UseMiddleware<ErrorHandlingMiddleware>();
+	app.UseMiddleware<RequestTimeLoggingMiddleware>();
+
+	app.UseSerilogRequestLogging();
+
+	if (app.Environment.IsDevelopment())
+	{
+		app.UseSwagger();
+		app.UseSwaggerUI();
+	}
+
+	app.UseHttpsRedirection();
+
+	// Register, login, etc.
+	app
+		.MapGroup("api/identity") // Add a prefix to Identity routes
+		.WithTags("Identity") // Place the endpoints under the Identity section
+		.MapIdentityApi<User>();
+
+	app.UseAuthorization();
+
+	app.MapControllers();
+
+	app.Run();
 }
-
-app.UseHttpsRedirection();
-
-// Register, login, etc.
-app
-	.MapGroup("api/identity") // Add a prefix to Identity routes
-	.WithTags("Identity") // Place the endpoints under the Identity section
-	.MapIdentityApi<User>();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+	Log.Fatal(ex, "Application startup failed.");
+}
+finally
+{
+	Log.CloseAndFlush();
+}
 
 public partial class Program { }
